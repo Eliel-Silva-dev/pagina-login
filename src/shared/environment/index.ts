4ae4f83ff996c335e328af14d7df696ef3a24784
Eliel-Silva-dev <eliellnr@hotmail.com>
2024-09-08 17:31:50 -0300

    feat: react login link url server

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,4 +1,5 @@
 export const Environment = {
   LIMITE_BUSCA_API: 50,
-  URL_BASE: 'http://localhost:3333',
+  URL_BASE: 'https://server-login.vercel.app/',
+  //URL_BASE: 'http://localhost:3333',
 };
